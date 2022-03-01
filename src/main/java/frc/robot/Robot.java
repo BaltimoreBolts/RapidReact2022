c05dc17a4232c2fae27c3881e510f0a261876b78
baltimorebolts6326 <baltimorebolts2019@gmail.com>
2022-03-01 18:31:19 -0500

    JJJ - updated to 2022.4.1; corrected refs

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,37 +2,74 @@ package frc.robot;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 
 public class Robot extends TimedRobot {
-  private final CANSparkMax mLeftDriveMotor1 = new CANSparkMax(1, MotorType.kBrushless);
-  private final CANSparkMax mLeftDriveMotor2 = new CANSparkMax(2, MotorType.kBrushless);
-  private final CANSparkMax mRightDriveMotor1 = new CANSparkMax(3, MotorType.kBrushless);
-  private final CANSparkMax mRightDriveMotor2 = new CANSparkMax(4, MotorType.kBrushless);
-  private final CANSparkMax[] mDriveMotors = {mLeftDriveMotor1, mLeftDriveMotor2, mRightDriveMotor1, mRightDriveMotor2};
-  
-  private final DifferentialDrive mRobotDrive = new DifferentialDrive(mRightDriveMotor1, mRightDriveMotor2);
-  private final Joystick mStick = new Joystick(0);
+  public CANSparkMax mLeftDriveMotor1;
+  public CANSparkMax mRightDriveMotor1;
+  public CANSparkMax mLeftDriveMotor2;
+  public CANSparkMax mRightDriveMotor2;
 
+  public Joystick mStick;
+  public DifferentialDrive mRobotDrive;
+  
   @Override
   public void robotInit() {
-    mRightDriveMotor1.setInverted(true); // right side is reverse of left
-    mRightDriveMotor2.setInverted(true);
-    for (int i=0; i<4; i++) {
-      // mDriveMotors[i].restoreFactoryDefaults();
-      mDriveMotors[i].setSmartCurrentLimit(40);
-      mDriveMotors[i].setIdleMode(CANSparkMax.IdleMode.kCoast);
-      // mDriveMotors[i].burnFlash();
-    }
-    mLeftDriveMotor1.follow(mLeftDriveMotor2);
-    mRightDriveMotor1.follow(mRightDriveMotor2);
+    mLeftDriveMotor1 = new CANSparkMax(1, MotorType.kBrushless);
+    mLeftDriveMotor2 = new CANSparkMax(2, MotorType.kBrushless);
+    mRightDriveMotor1 = new CANSparkMax(3, MotorType.kBrushless);
+    mRightDriveMotor2 = new CANSparkMax(4, MotorType.kBrushless);
+    
+    mRobotDrive = new DifferentialDrive(mRightDriveMotor1, mRightDriveMotor2);
+    mStick = new Joystick(0);
   }
+
+
+  @Override
+  public void robotPeriodic() {}
+
+
+  @Override
+  public void autonomousInit() {}
+
+
+  @Override
+  public void autonomousPeriodic() {}
+
+
+  @Override
+  public void teleopInit() {}
+
 
   @Override
   public void teleopPeriodic() {
     mRobotDrive.arcadeDrive(-mStick.getY(), mStick.getX());
   }
+
+  @Override
+  public void disabledInit() {}
+
+
+  @Override
+  public void disabledPeriodic() {}
+
+
+  @Override
+  public void testInit() {}
+
+
+  @Override
+  public void testPeriodic() {}
+
+
+  @Override
+  public void simulationInit() {}
+
+
+  @Override
+  public void simulationPeriodic() {}
+
 }

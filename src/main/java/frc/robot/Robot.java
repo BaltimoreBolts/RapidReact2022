43396f8523a4ae7a7d641840308d7b897fb6887b
baltimorebolts6326 <baltimorebolts2019@gmail.com>
2022-03-02 19:50:35 -0500

    JJJ - added basic autonomous

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
@@ -23,6 +24,14 @@ public class Robot extends TimedRobot {
   public Joystick mStick;
   public double mSpeed = 0.0;
   public double mTwist = 0.0;
+
+  public double wheelDia = 3.5; //inches
+  public double gearRatio = 4.89610; //nearly 5:1
+
+  public double autonStartTime;
+  public double autonWaitTime;
+  public double autonCurrentTime;
+  public double autonFinalPos;
   
   @Override
   public void robotInit() {
@@ -41,10 +50,10 @@ public class Robot extends TimedRobot {
     mLeftDriveMotor2.setSmartCurrentLimit(40);
     mRightDriveMotor1.setSmartCurrentLimit(40);
     mRightDriveMotor2.setSmartCurrentLimit(40);
-    mLeftDriveMotor1.setIdleMode(CANSparkMax.IdleMode.kCoast);
-    mLeftDriveMotor2.setIdleMode(CANSparkMax.IdleMode.kCoast);
-    mRightDriveMotor1.setIdleMode(CANSparkMax.IdleMode.kCoast);
-    mRightDriveMotor2.setIdleMode(CANSparkMax.IdleMode.kCoast);
+    mLeftDriveMotor1.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    mLeftDriveMotor2.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    mRightDriveMotor1.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    mRightDriveMotor2.setIdleMode(CANSparkMax.IdleMode.kBrake);
 
     mLeftDriveMotor1.follow(mLeftDriveMotor2);
     mRightDriveMotor1.follow(mRightDriveMotor2);
@@ -52,28 +61,56 @@ public class Robot extends TimedRobot {
     mLeftEncoder = mLeftDriveMotor1.getEncoder();
     mRightEncoder = mRightDriveMotor1.getEncoder();
     
-    //Convert raw encoder units to inches. Wheel DIA=3.5", Gear Ratio=4.89610:1
-    mLeftEncoder.setPositionConversionFactor((Math.PI * 3.5)/4.89610);
-    mRightEncoder.setPositionConversionFactor((Math.PI * 3.5)/4.89610);
+    //Convert raw encoder units to inches using PIxdia/gearRatio
+    mLeftEncoder.setPositionConversionFactor((Math.PI * wheelDia)/gearRatio);
+    mRightEncoder.setPositionConversionFactor((Math.PI * wheelDia)/gearRatio);
 
     mLeftDriveMotor1.setInverted(true);
     mLeftDriveMotor2.setInverted(true);
 
     mRobotDrive = new DifferentialDrive(mLeftMotors, mRightMotors);
     mStick = new Joystick(0);
+
+    autonWaitTime = 2;
+    autonFinalPos = -45;
   }
 
 
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+    //push values to dashboard here
+  }
 
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousInit() {
+    mRightEncoder.setPosition(0);
+    mLeftEncoder.setPosition(0);
+    autonStartTime = Timer.getFPGATimestamp();
+  }
 
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    autonCurrentTime = Timer.getFPGATimestamp();
+    //wait x time
+    if ((autonCurrentTime - autonStartTime) >= autonWaitTime) {
+      //shoot cargo
+
+      //move off tarmac at least 45" backwards (negative position)
+      if (mLeftEncoder.getPosition() > autonFinalPos){
+        //System.out.println(mLeftEncoder.getPosition());
+        mRobotDrive.arcadeDrive(-0.5, 0);
+      }
+      else {
+        mRobotDrive.arcadeDrive(0, 0);
+      }
+    }
+    else {
+      mRobotDrive.arcadeDrive(0, 0);
+    }
+    
+  }
 
 
   @Override
@@ -88,9 +125,6 @@ public class Robot extends TimedRobot {
     mSpeed = -mStick.getY() * (mStick.getThrottle()+1)/2;
     mTwist = mStick.getTwist() * (mStick.getThrottle()+1)/2;
     mRobotDrive.arcadeDrive(mSpeed, mTwist);
-
-    //System.out.println("Left:" +  mLeftEncoder.getPosition());
-    //System.out.println("Right:" + mRightEncoder.getPosition());
   }
 
   @Override
@@ -99,21 +133,4 @@ public class Robot extends TimedRobot {
 
   @Override
   public void disabledPeriodic() {}
-
-
-  @Override
-  public void testInit() {}
-
-
-  @Override
-  public void testPeriodic() {}
-
-
-  @Override
-  public void simulationInit() {}
-
-
-  @Override
-  public void simulationPeriodic() {}
-
 }
